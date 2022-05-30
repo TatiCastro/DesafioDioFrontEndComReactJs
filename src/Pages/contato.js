b0c6b222a3c981493018925af1230ddfe9451c5f
TatiCastro <88949545+TatiCastro@users.noreply.github.com>
2022-05-30 19:42:17 -0300

    Add files via upload

---
+++ src/Pages/contato.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Grid, Button, TextField } from '@material-ui/core/';
+
+const Contatos = () => {
+    return(
+        <>
+            <Grid container direction="row" xs={12}>
+                <TextField id="name" label="Name" fullWidth/>
+                <TextField id="message" label="Message" fullWidth/>
+            
+            </Grid>
+            <Button variant="contained" color="secondary">
+                Sent
+            </Button>
+        </>
+    )
+}
+
+export default Contatos;
---
+++ src/components/store/reducers/product.js
@@ -0,0 +1,136 @@
+const productsList = [
+    {
+      id_product: 1,
+      category: "Reconstrutor",
+      fk_idcategorys: 1,
+      name_product: "Kit Reconstrutor Cadivéu",
+      price: 350,
+      image: "./images/produtos/cadiveuR.jpg",
+      id_categorys: 1,
+      name_categorys: "Cadivéu",
+      selected: false
+    },
+    {
+      id_product: 2,
+      category: "Matizador",
+      fk_idcategorys: 1,
+      name_product: "Kit Matizador Cadivéu",
+      price: 280,
+      image: "./images/produtos/cadiveuM.jpg",
+      id_categorys: 1,
+      name_categorys: "Cadivéu",
+      selected: false
+    },
+    {
+      id_product: 9,
+      category: "Reconstrutor",
+      fk_idcategorys: 1,
+      name_product: "Kit Reconstrutor Amend",
+      price: 280,
+      image: "./images/produtos/amendR.jpg",
+      id_categorys: 1,
+      name_categorys: "Amend",
+      selected: false
+    },
+    {
+      id_product: 4,
+      category: "Matizador",
+      fk_idcategorys: 2,
+      name_product: "Kit Matizador Amend",
+      price: 235,
+      image: "./images/produtos/amendM.jpg",
+      id_categorys: 2,
+      name_categorys: "Amend",
+      selected: false
+    },
+    {
+      id_product: 10,
+      category: "Reconstrutor",
+      fk_idcategorys: 2,
+      name_product: "Kit Reconstrutor Joico",
+      price: 400,
+      image: "./images/produtos/joicoR.jpg",
+      id_categorys: 2,
+      name_categorys: "Joico",
+      selected: false
+    },
+    {
+      id_product: 11,
+      category: "Matizador",
+      fk_idcategorys: 2,
+      name_product: "Kit Matizador Joico",
+      price: 385,
+      image: "./images/produtos/joicoM.jpg",
+      id_categorys: 2,
+      name_categorys: "Joico",
+      selected: false
+    },
+    {
+      id_product: 6,
+      category: "Reconstrutor",
+      fk_idcategorys: 3,
+      name_product: "Kit Reconstrutor Kerastáse",
+      price: 439,
+      image: "./images/produtos/kerastaseR.jpg",
+      id_categorys: 3,
+      name_categorys: "Kerastáse",
+      selected: false
+    },
+    {
+      id_product: 7,
+      category: "Matizador",
+      fk_idcategorys: 3,
+      name_product: "Kit Matizador Kerastáse",
+      price: 390,
+      image: "./images/produtos/kerastaseM.jpg",
+      id_categorys: 3,
+      name_categorys: "Kerastáse",
+      selected: false
+    },
+    {
+      id_product: 3,
+      category: "Reconstrutor",
+      fk_idcategorys: 4,
+      name_product: "Kit Reconstrutor Loreal Expert",
+      price: 380,
+      image: "./images/produtos/expertR.jpg",
+      id_categorys: 4,
+      name_categorys: "Loreal Expert",
+      selected: false
+    },
+    {
+      id_product: 5,
+      category: "Matizador",
+      fk_idcategorys: 4,
+      name_product: "Kit Matizador Loreal Expert",
+      price: 365,
+      image: "./images/produtos/expertM.jpg",
+      id_categorys: 4,
+      name_categorys: "Loreal Expert",
+      selected: false
+    },
+    {
+      id_product: 8,
+      category: "Reconstrutor",
+      fk_idcategorys: 5,
+      name_product: "Kit Reconstrutor Trivit",
+      price: 295,
+      image: "./images/produtos/trivitR.jpg",
+      id_categorys: 5,
+      name_categorys: "Trivit",
+      selected: false
+    },
+    {
+      id_product: 12,
+      category: "Matizador",
+      fk_idcategorys: 5,
+      name_product: "Kit Matizador Trivit",
+      price: 275,
+      image: "./images/produtos/trivitM.jpg",
+      id_categorys: 5,
+      name_categorys: "Trivit",
+      selected: false
+    },
+];
+
+export default productsList;
